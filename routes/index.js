6b19286b9bbfaf78e51e720da4bb3e2f2caec693
Gordon Pimblott <g.pimblott@kainos.com>
2019-11-20 22:57:52 +0000

    Allow dynamic setting of bulk password

--- routes/index.js
+++ routes/index.js
@@ -80,26 +80,32 @@ router.get('/upload-account-csv' , security.isAuthenticatedAdmin, (req, res, nex
 });
 
 router.post('/upload-account-csv', security.isAuthenticatedAdmin, upload.single('csvfile'), (req, res, next) => {
+    let password='';
     csv
         .parseFile(req.file.path)
         .on('error', error => console.error(error))
         .on('data', row => {
 
-            accountDao.lookupRole( row[2])
-                .then( result => {
-                    let username = (String(row[0]).charAt(0) + row[1]).toLowerCase();
-                    let account = {
-                        username: username,
-                        role: result[0].id,
-                        firstname: row[0],
-                        surname: row[1],
-                        email: row[3],
-                        password: 'letmein2019'
-                    }
+            if( row[0]==="Password") {
+                password=row[1];
+                console.log("Found password:" + password);
+            } else {
+                accountDao.lookupRole(row[ 2 ])
+                    .then(result => {
+                        let username = (String(row[ 0 ]).charAt(0) + row[ 1 ]).toLowerCase();
+                        let account = {
+                            username: username,
+                            role: result[ 0 ].id,
+                            firstname: row[ 0 ],
+                            surname: row[ 1 ],
+                            email: row[ 3 ],
+                            password: password
+                        }
 
-                    console.log(account);
-                    accountDao.addAccount( account );
-                })
+                        console.log(account);
+                        //accountDao.addAccount(account);
+                    })
+            }
 
         })
         .on('end', rowCount => {
